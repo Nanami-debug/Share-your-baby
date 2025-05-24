7682d1c6c6febf32a6985bdc8e38decaa2f7ee15
1ZHOUJIAYI <150349546+1ZHOUJIAYI@users.noreply.github.com>
2025-05-24 23:58:36 +0800

    Add files via upload

---
+++ Dao/Database/DBHelper.cs
@@ -0,0 +1,52 @@
+﻿using MySql.Data.MySqlClient;
+using static Dapper.SimpleCRUD;
+
+namespace Dapper
+{
+    public class DBHelper
+    {
+        private static string Conn;
+
+        public static void Init(string conn)
+        {
+            Conn = conn;
+            using (var db = DBHelper.GetConn())
+            {
+                db.Query("SELECT 1");
+            }
+        }
+
+        public static bool IsInit()
+        {
+            return !string.IsNullOrEmpty(Conn);
+        }
+
+#if SQLITE
+        public static SQLiteConnection GetConn()
+        {
+            var connection = new SQLiteConnection(Conn);
+            SimpleCRUD.SetDialect(Dialect.SQLite);
+            connection.Open();
+            return connection;
+        }
+#elif SQLSERVER
+        public static SqlConnection GetConn()
+        {
+            var connection = new SqlConnection(Conn);
+            SimpleCRUD.SetDialect(Dialect.SQLite);
+            connection.Open();
+            return connection;
+        }
+#elif MYSQL
+
+        public static MySqlConnection GetConn()
+        {
+            var connection = new MySqlConnection(Conn);
+            SimpleCRUD.SetDialect(Dialect.MySQL);
+            connection.Open();
+            return connection;
+        }
+
+#endif
+    }
+}
---
+++ Dao/Database/Extensions.cs
@@ -0,0 +1,43 @@
+﻿using MySql.Data.MySqlClient;
+using System.Data;
+
+namespace Dapper
+{
+    public static class Extensions
+    {
+        public static DataTable QueryTable(this IDbConnection cnn, string sql)
+        {
+#if SQLITE
+                using (SQLiteCommand command = new SQLiteCommand(sql, (SQLiteConnection)cnn))
+                {
+                    using (SQLiteDataAdapter adapter = new SQLiteDataAdapter(command))
+                    {
+                        DataTable dataTable = new DataTable();
+                        adapter.Fill(dataTable);
+                        return dataTable;
+                    }
+                }
+#elif SQLSERVER
+                using (SqlCommand command = new SqlCommand(sql, (SqlConnection)cnn))
+                {
+                    using (SqlDataAdapter adapter = new SqlDataAdapter(command))
+                    {
+                        DataTable dataTable = new DataTable();
+                        adapter.Fill(dataTable);
+                        return dataTable;
+                    }
+                }
+#elif MYSQL
+            using (MySqlCommand command = new MySqlCommand(sql, (MySqlConnection)cnn))
+            {
+                using (MySqlDataAdapter adapter = new MySqlDataAdapter(command))
+                {
+                    DataTable dataTable = new DataTable();
+                    adapter.Fill(dataTable);
+                    return dataTable;
+                }
+            }
+#endif
+        }
+    }
+}
